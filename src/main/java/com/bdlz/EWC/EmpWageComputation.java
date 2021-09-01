4f8aacb194fac32f372cff717ebaaf932a765b4d
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 14:59:03 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/EWC/EmpWageComputation.java
@@ -0,0 +1,7 @@
+package com.bdlz.EWC;
+
+public class EmpWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage Computation Program");
+    }
+}
