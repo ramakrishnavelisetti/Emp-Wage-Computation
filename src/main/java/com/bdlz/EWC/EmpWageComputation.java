fb2bdc79a0f031b7e6641d66916b669656d08672
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 15:08:42 +0530

    Added: Emp DailyWage

--- src/main/java/com/bdlz/EWC/EmpWageComputation.java
+++ src/main/java/com/bdlz/EWC/EmpWageComputation.java
@@ -3,11 +3,16 @@ package com.bdlz.EWC;
 public class EmpWageComputation {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage =0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("employee is Present");
-        } else {
-            System.out.println("employee is Absent");
-        }
+            empHrs = 8;
+        } else
+            empHrs = 0;
+
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
     }
 }
