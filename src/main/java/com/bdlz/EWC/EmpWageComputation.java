79b49a4ca4d2fc62ff9bb335ea9c5e1e1c614af8
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 15:03:49 +0530

    Added: Emp Present or Absent

--- src/main/java/com/bdlz/EWC/EmpWageComputation.java
+++ src/main/java/com/bdlz/EWC/EmpWageComputation.java
@@ -2,6 +2,12 @@ package com.bdlz.EWC;
 
 public class EmpWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage Computation Program");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is Present");
+        } else {
+            System.out.println("employee is Absent");
+        }
     }
 }
